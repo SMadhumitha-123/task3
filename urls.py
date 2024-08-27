df9927b1c738839350b1a48eda53c8868c70fe38
SMadhumitha-123 <madhu.nandhu0930@gmail.com>
2024-08-27 18:58:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,32 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from employees import views as employee_views
 
 urlpatterns = [
-    path('', views.employee_list, name='employee_list'),  # Display all employees
-    path('add/', views.add_employee, name='add_employee'),  # Add new employee (admin only)
-    path('delete/<int:employee_id>/', views.delete_employee, name='delete_employee'),  # Delete employee (admin only)
-    path('signup/', views.sign_up, name='signup'),  # User sign-up
-    path('',views.home,name='home'),
-]
+    path('admin/', admin.site.urls),
+    path('employees/', include('employees.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', employee_views.sign_up, name='sign_up'),
+    path('', employee_views.home, name='home'),  # Home page URL
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
